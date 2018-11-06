411508015ec8c560eb272d9d0b35e3f43cdf9226
Cristopher Rosales <ichneutae@gmail.com>
2018-11-06 03:11:05 -0300

    Improved NavBar and added Images for Skills

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,19 @@
 $(function () {
-    $('.headerTitle').hover(function () {
-        $('h2', this).fadeIn(350);
-    }, function () {
-
-        $('h2', this).fadeOut(5);
+    //NavBar 
+    
+    $(window).scroll(function() {
+        $('#mainNav').toggleClass('bg-navbar-scrolled', $(this).scrollTop() > 500)
+        $('.js-scroll-trigger').toggleClass('scrolled-nav-link', $(this).scrollTop() > 500)
     });
 
-    $('.box').hover(function () {
-        let bgProp = $(this).css('background-image');
-        console.log(bgProp);
-        let url = bgProp.substring(bgProp.indexOf('url'));
-        console.log(url);
-        $(this).css("background-image", url);
-    }, function () {
-        let url = $(this).css('background-image');
-        let bgProp = 'linear-gradient(rgba(20, 20, 20, 0.5), rgba(20, 20, 20, 0.5)), ' + url;
-        $(this).css('background-image', bgProp);
-    });
+
+    // ScrollReveal
+    ScrollReveal({ reset: true });
+    ScrollReveal().reveal('.dash-center-name', { delay: 500 });
+    ScrollReveal().reveal('#skill-1', { delay: 200 });
+    ScrollReveal().reveal('#skill-2', { delay: 400 });
+    ScrollReveal().reveal('#skill-3', { delay: 600 });
+    ScrollReveal().reveal('#skill-4', { delay: 800 });
     // Scroll Smooth
     $('a[href*="#"]') // Select all links with #
         // Remove links that don't actually link to anything
